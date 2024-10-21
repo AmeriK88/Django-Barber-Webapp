a44947e928027970d13afa14708cfc7e01d2a316
Jose <lanzaltura17@gmail.com>
2024-10-21 20:49:04 +0100

    fix: corrección url - info vulnerable

---
+++ generateqr.py
@@ -0,0 +1,22 @@
+import qrcode
+
+# URL a la que se redirigirá el código QR
+url = 'http://tu_dirección_IP:8000/register/' 
+
+# Crear una instancia del generador de QR
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# Añadir la URL al QR
+qr.add_data(url)
+qr.make(fit=True)
+
+# Crear una imagen del QR
+img = qr.make_image(fill='black', back_color='white')
+
+# Guardar la imagen del QR
+img.save('registro_qr.png')
