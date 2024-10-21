4fea9459e3644f77ee54d57cf4d77c8e5cb170b9
Jose <lanzaltura17@gmail.com>
2024-10-22 00:19:24 +0100

    feat: implementación de fechas completas deshabilitadas junto con mejora de UI y CSS

--- static/js/disableDates.js
+++ static/js/disableDates.js
@@ -1,24 +1,33 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     const inputFecha = document.querySelector('input[name="fecha"]');
+    const inputHora = document.querySelector('select[name="hora"]');
 
     if (inputFecha) {
-        const today = new Date().toISOString().split('T')[0]; 
-        inputFecha.setAttribute('min', today);  // Fecha mínima: hoy
-
-        // Deshabilitar las fechas ocupadas
-        inputFecha.addEventListener('focus', function() {
+        inputFecha.addEventListener('focus', () => {
             const disabledDates = fechasOcupadas.map(fecha => new Date(fecha).toISOString().split('T')[0]);
 
-            inputFecha.addEventListener('input', function() {
+            inputFecha.addEventListener('input', () => {
                 const selectedDate = inputFecha.value;
 
-                // Si la fecha seleccionada está en la lista de fechas ocupadas, mostrar alerta y limpiar el campo
                 if (disabledDates.includes(selectedDate)) {
                     alert("La fecha seleccionada está completamente reservada. Por favor, selecciona otra.");
-                    inputFecha.value = '';  // Limpiar selección
+                    inputFecha.value = '';
+                } else {
+                    const horasOcupadas = horasOcupadasPorFecha[selectedDate] || [];
+
+                    Array.from(inputHora.options).forEach(option => {
+                        option.disabled = false;
+                    });
+
+                    horasOcupadas.forEach(hora => {
+                        Array.from(inputHora.options).forEach(option => {
+                            if (option.value === hora) {
+                                option.disabled = true;
+                            }
+                        });
+                    });
                 }
             });
         });
     }
 });
-``
